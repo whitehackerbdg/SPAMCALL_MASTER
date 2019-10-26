e039372ceab8a238addd8af8c150176786e1c930
whitehackerbdg <57010748+whitehackerbdg@users.noreply.github.com>
2019-10-26 22:58:09 +0700

    Add files via upload

---
+++ INSTALL.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Selamat Datang Di Tools Ini"
+sleep 2
+echo "Tools By White Hacker Bandung"
+sleep 2
+echo "Tools Ini Untuk Spam Call"
+sleep 2
+echo "Warning!!! Jangan Salah Gunakan Script"
+sleep 5
+          cd
+          pkg update && pkg upgrade
+          pkg install php
+          pkg install git
+          git clone https://github.com/whitehackerbdg/masterspamcall
+echo "Script Berhasil Di Install"
+sleep 3
